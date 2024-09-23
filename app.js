6e88042fd93cb6c5235a97bc77f709d9ffe32acb
Yao Xian Phang <80228647+yaoxianphang@users.noreply.github.com>
2024-09-23 15:10:01 +1000

    Update app.js

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static("dist"));
 
 app.get("/health", (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw "error...  ";
-  res.send("ok");
+  res.send("o
 });
 
 app.listen(PORT, () => {
